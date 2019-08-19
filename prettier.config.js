23131305b4c39d14bdec033b55de6110be0ca643
RFS4ever <RFS4ever@users.noreply.github.com>
2019-08-19 23:58:35 +0800

    Add prettier, husky, lint-staged packages
    * Add git pre-commit hook to use prettier autoformat support files
    * Add 'prettier.config.js' config file
    * Add '.prettierignore' to skip vendor directory

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
